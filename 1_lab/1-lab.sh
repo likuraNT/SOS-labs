4b759d680da5ecb4ec631f118bff7f7ecd49e900
likuraNT <claterfunk@gmail.com>
2024-12-04 20:14:19 +0300

    first update

--- 1_lab/1-lab.sh
+++ 1_lab/1-lab.sh
@@ -3,7 +3,7 @@
 function __users__
 {
 	echo "Users list:"
-	awk -F: '{print $1, $6 }' /etc/passwd | sort
+	awk -F':' '{print $1, $6 }' /etc/passwd | sort
 }
 
 function __processes__
@@ -39,14 +39,14 @@ function __help__
 	echo "Using: $0 [Options]"
 	echo "Options:"
 	echo " -u, --users		Output a list of users and their home directories."
-	echo " -p, --processes		Output a list of running processes, sorted by ID."
+	echo " -p, --processes          Output a list of running processes, sorted by ID."
 	echo " -h, --help		Display help menu."
 	echo " -l PATH, --log PATH	Log output to the file."
-	echo " -e PATH, --errors PATH 	Log errors to the file."
+	echo " -e PATH, --errors PATH	Log errors to the file."
 }
 
-LOG_PATH=''
-ERR_PATH=''
+LOG_PATH=""
+ERR_PATH=""
 while getopts ":uphl:e:-:" opt; do
 	case $opt in
 	u) __users__ ;;
@@ -61,7 +61,7 @@ while getopts ":uphl:e:-:" opt; do
 			help) __help__; exit 0 ;;
 			log) LOG_PATH=${!OPTIND}"; OPTIND=$(( $OPTIND + 1)) ;;
 			errors) ERR_PATH=${!OPTIND}"; OPTIND=$(( $OPTIND +1)) ;;
-			*) echo "Unknown option --${OPTARG}" >$2; exit 1 ;;
+			*) echo "Unknown option --${OPTARG}" >&2; exit 1 ;;
 		esac ;;
 	\?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
 	:) echo "Option -${OPTARG} needs an argument." >&2; exit 1 ;;
